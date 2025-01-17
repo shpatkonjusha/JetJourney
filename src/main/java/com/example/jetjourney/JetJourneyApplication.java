d394cc4b9c7ebde56bbc142dde93ad5f7e7dcc33
shpat <shpatkonjusha@gmail.com>
2025-01-17 15:21:03 +0100

    last steup

---
+++ src/main/java/com/example/jetjourney/JetJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.jetjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetJourneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JetJourneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jetjourney/JetJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jetjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetJourneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
